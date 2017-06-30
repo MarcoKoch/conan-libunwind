eb06285e9bcc94d28ae550492cd878b2c825cf61
Marco Koch <marco-koch@t-online.de>
2017-07-01 00:33:06 +0200

    Upload master branch to testing instead of stable

--- build.py
+++ build.py
@@ -2,6 +2,7 @@ from conan.packager import ConanMultiPackager
 
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager(username="marcokoch", channel="testing")
+    builder = ConanMultiPackager(username="marcokoch", channel="testing", \
+        stable_branch_pattern="^v[0-9]+(\\.[0-9])*")
     builder.add_common_builds(shared_option_name="libunwind:shared")
     builder.run()
