42ded920d1cfbd24e36fb6f4a668aa8e88b35ca2
Marco Koch <marco-koch@t-online.de>
2017-06-30 23:59:47 +0200

    Finally fix the cross-building problem
    There were a couple of third party bugs involved:
    - libunwind's configure script tries to use the wrong compiler, so we have to
      force the right one
    - libunwind's build system creates a broken symlink when cross-building shared
      libraries
    - conan's AutoToolsBuildEnvironment does not consider cross-building from
      x86_64 to x86 as cross-building and thus does not specify the correct flags
      to the configure script
    
    The workaround for the latter also addresses one of the build problems on
    Windows:
    - conan's AutoToolsBuildEnvironment insists to use a forward slash as path
      delimiter when running the configure script

--- conanfile.py
+++ conanfile.py
@@ -1,6 +1,5 @@
 from conans import ConanFile, AutoToolsBuildEnvironment, tools
-from glob import glob
-import os, platform, re
+import os, platform
 
 
 class LibunwindConan(ConanFile):
@@ -18,7 +17,8 @@ class LibunwindConan(ConanFile):
 "enable_coredump": [True, False],
 "enable_ptrace": [True, False],
 "enable_setjmp": [True, False],
-"enable_documentation": [True, False],
+# Not supported for now, as we don not have a latex2man package
+#"enable_documentation": [True, False],
 "enable_cxx_exceptions": [True, False],
 "enable_debugframe": [True, False],
 "enable_block_signals": [True, False],
@@ -31,7 +31,6 @@ shared=False
 enable_coredump=False
 enable_ptrace=False
 enable_setjmp=False
-enable_documentation=False
 enable_cxx_exceptions=False
 enable_debugframe=False
 enable_block_signals=False
@@ -44,6 +43,33 @@ enable_minidebuginfo=False
     source_url = "http://download.savannah.nongnu.org/releases/libunwind/%s.tar.gz" % source_archive_name
     local_install_path = "install"
 
+    
+    def _get_build_environment_str(self):
+        if tools.os_info.is_linux:
+            return "%s-unknown-linux" % platform.machine()
+        elif tools.os_info.is_windows:
+            return "%s-windows" % platform.machine()
+        elif tools.os_info.is_macos:
+            return "%s-macos" % platform.machine()
+        else:
+            return platform.machine()
+    
+    
+    def _get_target_environment_str(self):
+        if self.settings.arch == "x86":
+            arch = "i686"
+        else:
+            arch = self.settings.arch
+        
+        if self.settings.os == "Linux":
+            return "%s-unknown-linux" % arch
+        elif self.settings.os == "Windows":
+            return "%s-windows" % arch
+        elif self.settings.os == "Macos":
+            return "%s-macos" % arch
+        else:
+            return arch
+
 
     def source(self):
         self.output.info("Downloading sources from %s" % self.source_url)
@@ -51,7 +77,7 @@ enable_minidebuginfo=False
 
 
     def build(self):
-        build_env = AutoToolsBuildEnvironment(self)
+        build_env = AutoToolsBuildEnvironment(self)        
         configure_options = ["--prefix="]
         
         if self.settings.build_type == "Debug":
@@ -71,8 +97,9 @@ enable_minidebuginfo=False
         if not self.options.enable_setjmp:
             configure_options.append("--disable-setjmp")
         
-        if not self.options.enable_documentation:
-            configure_options.append("--disable-documentation")
+        # Not supported for now, as we don not have a latex2man package
+        #if not self.options.enable_documentation:
+        #    configure_options.append("--disable-documentation")
         
         if self.options.enable_cxx_exceptions:
             configure_options.append("--enable-cxx-exceptions")
@@ -91,47 +118,52 @@ enable_minidebuginfo=False
     
         if self.options.enable_minidebuginfo:
             configure_options.append("--enable-minidebuginfo")
+            
+        # Conan does not consider cross-building from x86_64 to x86 as such.
+        # This causes trouble though, as the configure script screws up
+        # if it has no exact host specified in such situations.
+        configure_options.append("--build=%s" % self._get_build_environment_str())
+        configure_options.append("--host=%s" % self._get_target_environment_str())
+        configure_options.append("--target=%s" % self._get_target_environment_str())
+        
+        # HACK: If crosscompiling from x86_64 to x86, libunwind's configure script
+        # tries to use the 64 bit compiler even though host and target platform
+        # are specifed correctly. We manually add the -m32 flag to the CFLAGS
+        # to enforce the correct compiler.
+        if self.settings.arch == "x86" and (self.settings.compiler == "gcc" or \
+                self.settings.compiler == "clang" or self.settings.compiler == "apple-clang"):
+            configure_options.append("CFLAGS=-m32")
         
         with tools.chdir(self.source_archive_name):
             self.output.info("Configuring libunwind")
-                 
-            # Conan does not consider cross-building from x86_64 to x86 as such.
-            # This causes trouble, though, as the configure script screws up
-            # if it has no exact target specified in such situations.
-            # Also, there seems to be a bug in conan, which causes the build to
-            # fail on windows if no explicit configuration path is specified
-            # (conan calls the configure script with ./configure)
-            if platform.machine() == "x86_64" and self.settings.arch == "x86":
-                build_env.configure(args=configure_options, target="i686",
-                    configure_dir=os.getcwd())
-            else:
-                build_env.configure(args=configure_options, configure_dir=os.getcwd())
+            
+            # HACK: We can't use build_env.configure() here as that neither works on
+            # Windows, nor does it play nice with libunwind's configure script
+            # in cross bulding setups. Instead we call the configure script directly.
+            self.run(os.path.join(os.path.curdir, "configure %s" % " ".join(configure_options)))
             
             self.output.info("Building libunwind")
             build_env.make()
             
             self.output.info("Doing a local install for packaging")
-            build_env.make(["install", "prefix=%s" % os.path.join(os.getcwd(), os.pardir, self.local_install_path)])
-            
-        # HACK: There seems to be a bug in libunwind's build system that creates a
-        # broken symlink libunwind-generic.a when it is actually supposed to
-        # create shared libraries. Conan drops out with an error when it tries
-        # to copy that link. As a simple fix, we delete the link here.
+            build_env.make(["install", "prefix=%s" % os.path.join(os.getcwd(), \
+                os.pardir, self.local_install_path)])
+        
+        # HACK: There seems to be a bug in libunwind's build system that causes
+        # a broken symlink to be created when configured with --enable-shared.
+        # This causes conan to drop out with an error when it tries to copy
+        # that link during package(). The easiest workaround should be to just
+        # delete that nonsense.
         if self.options.shared:
             broken_link = os.path.join(self.local_install_path, "lib", "libunwind-generic.a")
             if os.path.islink(broken_link):
-                self.output.info("HACK: Removing broken symlink %s" % broken_link)
+                self.output.info("HACK: Deleting broken symlink %s" % broken_link)
                 os.unlink(broken_link)
         
 
     def package(self):
         self.copy("*", dst=".", src=self.local_install_path, links=True)
         self.copy("COPYING", dst="licenses", src=self.source_archive_name)
-        
-        # libunwind does not install the libunwind header when cross-building.
-        if tools.cross_building(self.settings) or \
-                (platform.machine() == "x86_64" and self.settings.arch == "x86"):
-            self.copy("libunwind.h", dst="include", src=os.path.join(self.source_archive_name, "include"))
 
 
     def package_info(self):
