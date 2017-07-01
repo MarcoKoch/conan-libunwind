fe1d0606215e3c49a186d719f0c85d4034081a08
Marco Koch <marco-koch@t-online.de>
2017-07-02 01:59:39 +0200

    Upload release tags to stable, everything else to testing

--- .travis/run.sh
+++ .travis/run.sh
@@ -11,11 +11,17 @@ set -x
 #    pyenv activate conan
 #fi
 
-# Only upload packages from the master or stable release branches.
+# Only upload packages from release branches
 # This prevents us from spamming the 'testing' repo with packages from
 # feature branches.
-if ! [[ $TRAVIS_PULL_REQUEST = "false" && ($TRAVIS_BRANCH = "master" || $TRAVIS_TAG =~ ^v[0-9]+(\.[0-9]+)*-[0-9]+$) ]] ; then
+if ! [[ $TRAVIS_PULL_REQUEST = "false" && $TRAVIS_BRANCH =~ ^v[0-9]+(\.[0-9]+)*$ ]] ; then
     unset CONAN_UPLOAD
+fi
+
+# Upload release tags to the stable channel and everything else to the testing
+# channel.
+if ! [[ $TRAVIS_TAG =~ ^v[0-9]+(\.[0-9]+)*-[0-9]+$ ]] ; then
+    export CONAN_STABLE_BRANCH_PATTERN="a^" # match nothing
 fi
 
 python build.py
